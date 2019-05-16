2e3b01c192e5e6e5bd74c8a8fc26f707e3f07eed
phamhuuan <phaman3899@gmail.com>
2019-05-16 22:28:45 +0700

    deleted:    "\r" 	deleted:    "\r.pub" 	deleted:    main.exe 	deleted:    main115103052019.c 	deleted:    main2 	deleted:    mainbackup 	deleted:    mainbackup.c 	deleted:    mystyle.css 	deleted:    test 	deleted:    test.c

---
+++ src/test2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long countLine(FILE *f){
+  char c; long long count = 0;
+  while((c = fgetc(f)) != EOF) /*if(c == '\n')*/ count++;
+  return count + 1;
+}
+
+int main(){
+  int sum = 0;
+  FILE *f;
+  f = fopen("add.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("closeWindow.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("delete.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("history.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("extern.h", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("library.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("main.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("message.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("myCSS.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("mystyle.css", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("restore.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("search.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("setTextView.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("setting.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("team.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("textToBtree.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  f = fopen("tutorial.c", "r");
+  sum += countLine(f);
+  fclose(f);
+  printf("%d\n", sum);
+  return 0;
+}
